7566627c6ae1909ee28d4bc8e3d55d49620dc0d6
viks17 <90504865+viks17@users.noreply.github.com>
2021-11-22 10:51:25 +0500

    Add files via upload

---
+++ 2 lab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d;
+            double F;
+            Console.WriteLine("Введите a"); 
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b");  
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите c"); 
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите d"); 
+            d = Convert.ToDouble(Console.ReadLine());
+            F = (a * b + c * d) + (a * d - b * c);
+            Console.Write("Результат: " + F);
+            Console.ReadKey();
+
+        }
+    }
+}
